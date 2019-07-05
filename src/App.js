8f8ada4c7c45959c1efb4394cd3b38d22b87dcf0
hoxi777 <jtho1015@gmail.com>
2019-07-05 22:20:06 +0900

    study grid & flex layout

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+import Grid from "./Example/grid"
+
+export default () => {
+  return (
+    <>
+      <div className="App-wrap ">
+
+        <Grid></Grid>
+
+      </div>
+    </>
+  )
+}
+
+//git remote add origin git@github.com:hoxi777/study-fontend.git
---
+++ src/Example/grid.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './grid.css';
+
+export default () => (
+    <>
+        <div className="wrap">
+            <div className="one">
+                <div className="flex">하나</div>
+                <div className="flex">둘</div>
+                <div className="flex">셋</div>
+                <div className="noneflex">넷</div>
+            </div>
+            <div className="two">two</div>
+            <div className="three">three</div>
+            <div className="four">four</div>
+            <div className="five">five</div>
+        </div>
+
+        <div>
+            
+        </div>
+        </>
+);
